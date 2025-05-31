5306f3dbd90b2c573308974f92d866a699bcbfc8
VipulVerma77 <vipulverma4577@gmail.com>
2025-05-31 16:48:14 +0530

    signin

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -14,7 +14,7 @@ export async function clientLoader() {
         const existingUser = await getExistingUser(user.$id);
 
         if(existingUser?.status === 'user') {
-            return redirect('/');
+            return redirect('/dashboard');
         }
 
         return existingUser?.$id ? existingUser : await storeUserData();
